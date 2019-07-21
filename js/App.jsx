4640c740ef4cadb36fbb3523876663f2dcaba9e0
Marek Stankiewicz <marekstankiewicz6@wp.pl>
2019-07-21 13:20:49 +0200

    Postępy 21.07.19r godz 13:20

--- js/App.jsx
+++ js/App.jsx
@@ -5,7 +5,28 @@ import ReactDOM from 'react-dom';
 class Index extends React.Component {
 
     state = {
-        options: ["1", "2", "3", "4"],
+        options: [
+            "Tylko dwie rzeczy są nieskończone: wszechświat oraz ludzka głupota, choć nie jestem pewien co do tej pierwszej",
+            "Nie kłóć się z idiotą, bo najpierw sprowadzi cię do swojego poziomu, a potem pokona doświadczeniem",
+            "Szansa na pójście do sklepu po chleb i wyjście tylko z chlebem jest jak trzy miliardy do jednego",
+            "Skąpcy nie są przyjemnymi partnerami do życia, ale za to wspaniale nadają się na przodków",
+            "Wszyscy słyszeliśmy, że milion małp przy milionie klawiatur mogło wyprodukować Dzieła Zebrane Szekspira; ale dzięki Internetowi wiemy, że to nieprawda",
+            "Cierpliwość jest czymś, za co podziwiamy kierowcę jadącego za nami, ale nie tego, który jedzie przed nami",
+            "Jedyną tajemnicą życia jest to, dlaczego piloci kamikaze nosili hełmy",
+            "Wiedza polega na tym, że mamy świadomość, iż pomidor to owoc. Mądrość polega na tym, że nie dodajemy go do sałatki owocowej",
+            "Zawsze pożyczaj pieniądze od pesymistów. Nie będzie spodziewał się ich zwrotu",
+            "Szaleństwo jest dziedziczne. Dostajesz na głowę z powodu swoich dzieci",
+            "Makler zachęcał mnie do zakupu akcji, które potroją swoją wartość każdego roku. Powiedziałem mu: \"W moim wieku nie kupuję nawet zielonych bananów",
+            "Chodzenie do kościoła nie czyni cię bardziej chrześcijaninem, niż stanie w garażu czyni cię samochodem",
+            "Jeśli kradniesz od jednego autora, to plagiat. Jeśli kradniesz od wielu, to są badania",
+            "Kiedy umrę, chcę odejść spokojnie, jak mój dziadek, we śnie. Nie chcę odchodzić krzycząc i wrzeszcząc, jak pasażerowie w jego samochodzie",
+            "Zanim mężczyzna zda sobie sprawę, że jego ojciec miał rację, ma już syna, który uważa, że ​​się myli",
+            "Poprosiłem Boga o rower, ale wiem, że Bóg nie działa w ten sposób. Ukradłem więc rower i poprosiłem o przebaczenie",
+            "Najpierw lekarz przekazał mi dobrą wiadomość: cierpiałem na chorobę, która zostanie nazwana moim nazwiskiem",
+            "Ludzie sądzący, że wiedzą wszystko, są bardzo irytujący dla tych z nas, którzy naprawdę wiedzą",
+            "Promocja jest czymś, czego nie potrzebujesz, po cenie, której nie możesz się oprzeć",
+            "Bank to miejsce, które pożyczy ci pieniądze, jeśli możesz udowodnić, że ich nie potrzebujesz",
+                ],
         option: null,
         value: "",
     };
@@ -41,13 +62,13 @@ class Index extends React.Component {
     render() {
         return (
             <div>
-                <button onClick={this.handleShowOption}>Losuj żart</button>
+                <p></p>
                 {this.state.option ? <h1>{this.state.option}</h1> : null }
+                <button onClick={this.handleShowOption}>Generuj</button>
                 <br/>
                 <input type="text" value={this.state.value} onChange={this.handleWriteJoke}/>
                 <button onClick={this.handleAddJoke}>Dodaj żart</button>
                 <br/>
-
             </div>
         )
     }
--- js/out.js
+++ js/out.js
@@ -9625,7 +9625,7 @@ var Index = function (_React$Component) {
         }
 
         return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Index.__proto__ || Object.getPrototypeOf(Index)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
-            options: ["1", "2", "3", "4"],
+            options: ["Tylko dwie rzeczy są nieskończone: wszechświat oraz ludzka głupota, choć nie jestem pewien co do tej pierwszej", "Nie kłóć się z idiotą, bo najpierw sprowadzi cię do swojego poziomu, a potem pokona doświadczeniem", "Szansa na pójście do sklepu po chleb i wyjście tylko z chlebem jest jak trzy miliardy do jednego", "Skąpcy nie są przyjemnymi partnerami do życia, ale za to wspaniale nadają się na przodków", "Wszyscy słyszeliśmy, że milion małp przy milionie klawiatur mogło wyprodukować Dzieła Zebrane Szekspira; ale dzięki Internetowi wiemy, że to nieprawda", "Cierpliwość jest czymś, za co podziwiamy kierowcę jadącego za nami, ale nie tego, który jedzie przed nami", "Jedyną tajemnicą życia jest to, dlaczego piloci kamikaze nosili hełmy", "Wiedza polega na tym, że mamy świadomość, iż pomidor to owoc. Mądrość polega na tym, że nie dodajemy go do sałatki owocowej", "Zawsze pożyczaj pieniądze od pesymistów. Nie będzie spodziewał się ich zwrotu", "Szaleństwo jest dziedziczne. Dostajesz na głowę z powodu swoich dzieci", "Makler zachęcał mnie do zakupu akcji, które potroją swoją wartość każdego roku. Powiedziałem mu: \"W moim wieku nie kupuję nawet zielonych bananów", "Chodzenie do kościoła nie czyni cię bardziej chrześcijaninem, niż stanie w garażu czyni cię samochodem", "Jeśli kradniesz od jednego autora, to plagiat. Jeśli kradniesz od wielu, to są badania", "Kiedy umrę, chcę odejść spokojnie, jak mój dziadek, we śnie. Nie chcę odchodzić krzycząc i wrzeszcząc, jak pasażerowie w jego samochodzie", "Zanim mężczyzna zda sobie sprawę, że jego ojciec miał rację, ma już syna, który uważa, że ​​się myli", "Poprosiłem Boga o rower, ale wiem, że Bóg nie działa w ten sposób. Ukradłem więc rower i poprosiłem o przebaczenie", "Najpierw lekarz przekazał mi dobrą wiadomość: cierpiałem na chorobę, która zostanie nazwana moim nazwiskiem", "Ludzie sądzący, że wiedzą wszystko, są bardzo irytujący dla tych z nas, którzy naprawdę wiedzą", "Promocja jest czymś, czego nie potrzebujesz, po cenie, której nie możesz się oprzeć", "Bank to miejsce, które pożyczy ci pieniądze, jeśli możesz udowodnić, że ich nie potrzebujesz"],
             option: null,
             value: ""
         }, _this.handleShowOption = function () {
@@ -9656,16 +9656,17 @@ var Index = function (_React$Component) {
             return _react2.default.createElement(
                 'div',
                 null,
-                _react2.default.createElement(
-                    'button',
-                    { onClick: this.handleShowOption },
-                    'Losuj \u017Cart'
-                ),
+                _react2.default.createElement('p', null),
                 this.state.option ? _react2.default.createElement(
                     'h1',
                     null,
                     this.state.option
                 ) : null,
+                _react2.default.createElement(
+                    'button',
+                    { onClick: this.handleShowOption },
+                    'Generuj'
+                ),
                 _react2.default.createElement('br', null),
                 _react2.default.createElement('input', { type: 'text', value: this.state.value, onChange: this.handleWriteJoke }),
                 _react2.default.createElement(
